6573ccd930c944a553b35b8a6d20932a1a72894f
flagimtoshi <petermehlhq@gmail.com>
2024-04-30 19:38:52 +0200

    new site tests

---
+++ createNav.js
@@ -0,0 +1,5 @@
+const nav = document.createElement('nav');
+nav.className = menu;
+const ul = document.createElement('ul');
+nav.append(ul)
+
---
+++ sidebar.js
@@ -0,0 +1,24 @@
+
+        // Selecting the iframe element 
+        var frame = document.getElementById("Iframe"); 
+        var sidebar = document.getElementById("Sidebar"); 
+          
+        // Adjusting the iframe height onload event 
+        frame.onload = function() 
+        // function execute while load the iframe 
+        { 
+          // set the height of the iframe as  
+          // the height of the iframe content 
+          frame.style.height =  
+          frame.contentWindow.document.body.scrollHeight + 'px'; 
+          sidebar.style.height =  
+          frame.contentWindow.document.body.scrollHeight + 'px'; 
+          
+           
+  
+         // set the width of the iframe as the  
+         // width of the iframe content 
+         frame.style.width  =  
+          frame.contentWindow.document.body.scrollWidth+'px'; 
+              
+        } 
